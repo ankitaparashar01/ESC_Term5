fbaf869883ffb71de2bb86b0d8086c9b6766063d
Ankita Parashar <ankitap2001.ap@gmail.com>
2022-07-04 12:55:42 +0800

    Update urls.py

--- djangosite/myproject/myapp/urls.py
+++ djangosite/myproject/myapp/urls.py
@@ -3,8 +3,11 @@ from . import views
 # from .views import *
 
 urlpatterns = [
-    path('', views.myfunctioncall, name="index"),
-    path('about', views.myfunctionabout, name="about"),
-    path('intro/<str:name>/<int:age>', views.intro, name="intro"),
-    path('myfirstpage', views.myfirstpage, name="myfirstpage")
+    # path('', views.myfunctioncall, name="index"),
+    # path('about', views.myfunctionabout, name="about"),
+    # path('intro/<str:name>/<int:age>', views.intro, name="intro"),
+    path('', views.ascenda, name="ascenda"),
+    path('hotellist', views.hotellist, name="hotellist"),
+    path('roomlist', views.roomlist, name="roomlist"),
+    path('confirmation', views.confirmation, name="confirmation")
 ]
