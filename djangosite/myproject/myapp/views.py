486a2605e0fe1add674679d53091761accb8175c
Ankita Parashar <ankitap2001.ap@gmail.com>
2022-07-04 12:55:47 +0800

    Update views.py

--- djangosite/myproject/myapp/views.py
+++ djangosite/myproject/myapp/views.py
@@ -2,19 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
-def myfunctioncall(request):
-    return HttpResponse("Hello World")
+# def myfunctioncall(request):
+#     return HttpResponse("Hello World")
 
-def myfunctionabout(request):
-    return HttpResponse("About Response")
+# def myfunctionabout(request):
+#     return HttpResponse("About Response")
 
-def intro(request, name, age):
-    mydictionary = {
-        "name" : name,
-        "age" : age
-    }
-    return JsonResponse(mydictionary)
+# def intro(request, name, age):
+#     mydictionary = {
+#         "name" : name,
+#         "age" : age
+#     }
+#     return JsonResponse(mydictionary)
 
-def myfirstpage(request):
+def ascenda(request):
     return render(request, 'index.html')
+
+def hotellist(request):
+    return render(request, 'hotellist.html')
+
+def roomlist(request):
+    return render(request, 'roomlist.html')
+
+def confirmation(request):
+    return render(request, 'confirmation.html')
 
