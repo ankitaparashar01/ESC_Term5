c012efc9cb4307a1af36e7799ea05ab59aac38fb
thoastyy <75476459+thoastyy@users.noreply.github.com>
2022-07-16 22:48:40 +0800

    To merge into master

--- djangosite/myproject/myapp/views.py
+++ djangosite/myproject/myapp/views.py
@@ -21,7 +21,7 @@ def ascenda(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect('/testapi/')
+            return HttpResponseRedirect('/hotellistings/')
     
     # if a GET (or any other method) we'll create a blank form
     else:
@@ -76,10 +76,9 @@ def transactionComplete(request):
 
 #hotel search based on chosesn location
 def testapi(request, destId):
-    destIdVar = destId
+    destIdVar = destId # access in HTML using --> <h1>dest uid: {{ destIdVar }}</h1>
     # dest_list_models = DestinationCat.objects.get(uid=destIdVar)
     dest_list_models = DestinationCat.objects.all()
-
 
     #dyanamic JSON Api search for specific destination: 
     jsonDestStr = str(destId)
@@ -90,27 +89,19 @@ def testapi(request, destId):
     # DESTINATION HERE IS HARDCODED!!
     # response1 = requests.get("https://hotelapi.loyalty.dev/api/hotels?destination_id=WD0M").json() # from Mock Static Data endpoints -> Static information of hotels belonging to a destination
 
-
      # set up pagination below:
     p = Paginator(response1, 3)
     page = request.GET.get('page',1)
     listings = p.get_page(page)
 
-
-    # response1 = requests.get('https://hotelapi.loyalty.dev/api/hotels?destination_id=WD0M').json() # from Mock Static Data endpoints -> Static information of hotels belonging to a destination
-    # response2 = requests.get('https://hotelapi.loyalty.dev/api/hotels/diH7').json() # chosen hotel is fullerton
-    # response3 = requests.get('https://hotelapi.loyalty.dev/api/hotels/diH7/price?destination_id=RsBU&checkin=2021-03-31&checkout=2021-04-01&lang=en_US&currency=SGD&partner_id=16&country_code=SG&guests=2').json()
-    # response4 = requests.get('https://hotelapi.loyalty.dev/api/hotels/prices?destination_id=RsBU&checkin=2021-03-31&checkout=2021-04-01&lang=en_US&currency=SGD&landing_page=&partner_id=16&country_code=SG&guests=2').json()
     context = {'response1':response1,
-    # 'response2':response2,
-    # 'response3':response3,
-    # 'respone4':response4
     'listings': listings,
     # 'dest_list': dest_list,
     'destIdVar':destIdVar,
     'dest_list_models':dest_list_models,
     }
-    return render(request,'testapi.html', context)
+
+    return render(request,'NEWhotellistings.html', context)
 
 def testapiRoomList(request, hotelName, hotelId, destId):
     #dyanamic JSON Api search for specific Hotel below: 
@@ -118,11 +109,11 @@ def testapiRoomList(request, hotelName, hotelId, destId):
     jsonHotelRoomBaseStr = str("https://hotelapi.loyalty.dev/api/hotels/")
     jsonReqestInput = jsonHotelRoomBaseStr + jsonHotelStr
     response2 = requests.get(jsonReqestInput).json() 
-    
 
     context = {'response2':response2,
     # 'hotelnamevar' : hotelnamevar,
     }
+
     return render(request,'roomlisttestapi.html', context)
 
 
