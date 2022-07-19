5169d7d7954b50ab671e1571380702a8d424495b
Ankita Parashar <ankitap2001.ap@gmail.com>
2022-07-19 15:58:29 +0800

    Update views.py

--- djangosite/myproject/myapp/views.py
+++ djangosite/myproject/myapp/views.py
@@ -1,27 +1,34 @@
 from multiprocessing import context
 from django.shortcuts import redirect, render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, response
 from typing import List
 from .models import *
 from .forms import *
 from django.core.paginator import Paginator
 import requests
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+
 
 
 #---------------------FOR LANDING PAGE FORM---------------------
 def ascenda(request):
-    
+
+    template_name = "index.html"
+    form_class = SearchHotelForm
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = DestinationCreationForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/hotellistings/')
+    # if request.method == 'POST':
+    #     # create a form instance and populate it with data from the request:
+    #     form = DestinationCreationForm(request.POST)
+    #     # check whether it's valid:
+    #     if form.is_valid():
+    #         # process the data in form.cleaned_data as required
+    #         # ...
+    #         # redirect to a new URL:
+    #         return HttpResponseRedirect('/hotellistings/')
+    def form_valid(self, form):
+        destination = form.cleaned_data.get("")
     
     # if a GET (or any other method) we'll create a blank form
     else:
