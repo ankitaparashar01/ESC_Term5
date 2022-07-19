596ad2e34d459b9fc1cf72bf016f5d7bc574e550
Ankita Parashar <ankitap2001.ap@gmail.com>
2022-07-20 00:36:04 +0800

    Update forms.py

--- djangosite/myproject/myapp/forms.py
+++ djangosite/myproject/myapp/forms.py
@@ -1,3 +1,5 @@
+from dataclasses import field
+from pyexpat import model
 from django import forms
 from .models import *
 
@@ -27,5 +29,26 @@ class SearchHotelForm(forms.ModelForm):
             "class": "form-control"
         }
     ))
-    rooms = forms.
+    rooms = forms.ChoiceField(widget=forms.Select(
+        choices=[0,1,2,3,4,5]
+    ))
+    adults = forms.ChoiceField(widget=forms.Select(
+        choices=[0,1,2,3,4,5]
+    ))
+    children = forms.ChoiceField(widget=forms.Select(
+        choices=[0,1,2,3,4,5]
+    ))
+
+    class Meta:
+        model = ListingItem
+        fields = ('destination', 'checkin_date', 'checkout_date', 'rooms', 'adults', 'children')
+
+    def clean(self):
+        cleaned_data = super(SearchHotelForm,self).clean()
+        checkin_date = cleaned_data.get("checkin_date")
+        checkout_date = cleaned_data.get("checkout_date")
+        if checkout_date < checkin_date:
+            raise forms.ValidationError("End date should be greater than start date.")
+
+
 
