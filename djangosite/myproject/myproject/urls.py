b208f861423df635c050b52f3f7ba76f419bfc5d
zzerodream <mengyi233@gmail.com>
2022-07-19 14:38:24 +0800

    fanyi's part

--- djangosite/myproject/myproject/urls.py
+++ djangosite/myproject/myproject/urls.py
@@ -14,11 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from myapp import views
+
 
 
 # adding urls from the app into the project in the urls.py but in the myproject
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('', include('myapp.urls'))
+#]
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls'))
+    # path('', views.myfunctioncall, name="index"),
+    # path('about', views.myfunctionabout, name="about"),
+    # path('intro/<str:name>/<int:age>', views.intro, name="intro"),
+    path('', views.ascenda, name="ascenda"),
+    path('hotellist/', views.all_listings, name="list-hotels"),
+    path("roomtype/<str:hotelname>", views.roomListPage, name='list-room-type'),
+    path('confirmation/', views.confirmation, name="confirmation"),
+    path('transaction-complete/', views.transactionComplete, name="transaction-complete"),
+    path('ajax/', views.ajax),
+    path('ajax2/', views.ajax2),
+    path('check-order/', views.check, name="check"),
+    path('display-order/', views.display)
 ]
