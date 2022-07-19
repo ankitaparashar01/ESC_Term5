eb69826641876adad2a3e07d00b408cf86aa19d9
Ankita Parashar <ankitap2001.ap@gmail.com>
2022-07-19 15:58:23 +0800

    Update forms.py

--- djangosite/myproject/myapp/forms.py
+++ djangosite/myproject/myapp/forms.py
@@ -11,3 +11,21 @@ class DestinationCreationForm(forms.ModelForm):
 # to ankita, you can add the form thingy below here! 
 # i will eventually delete DestinationCreationForm but for now im still using it
 # :>
+class SearchHotelForm(forms.ModelForm):
+    destination = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "placeholder": "Destination or Hotel",
+            "class": "form-control"
+        }))
+    checkin_date = forms.DateField(widget=forms.DateInput(
+        attrs={
+            "class": "form-control"
+        }))
+
+    checkout_date = forms.DateField(widget=forms.DateInput(
+        attrs={
+            "class": "form-control"
+        }
+    ))
+    rooms = forms.
+
