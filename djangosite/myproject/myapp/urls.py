2209d94ead946f8df29c6d04d0a1814f80b9364a
Ankita Parashar <ankitap2001.ap@gmail.com>
2022-07-26 14:26:30 +0800

    Update urls.py

--- djangosite/myproject/myapp/urls.py
+++ djangosite/myproject/myapp/urls.py
@@ -14,4 +14,9 @@ urlpatterns = [
     path('hotellistings/dest_id=<str:destId>/name:<str:hotelName>/id=<str:hotelId>', views.testapiRoomList, name="room-list-test-api"), #temporary, will be deleted after transition is settled
     # path('testapi/name:<str:hotelName>/id=<str:hotelId>', views.testapiRoomList, name="room-list-test-api"), #temporary, will be deleted after transition is settled
 
+    # trying out new url for the form submission temporary
+    path('submitmyform', views.submitmyform, name='submitmyform')
+    # path('myform2', views.myform2, name='myform2')
+    
+
 ]
