e95d4999d62acc0b48bff4dec1dfab79ab80e8bc
Ankita Parashar <ankitap2001.ap@gmail.com>
2022-07-26 19:29:24 +0800

    Update urls.py

--- djangosite/myproject/myapp/urls.py
+++ djangosite/myproject/myapp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # TO ADD: confirmation page should have specific room id
     path('confirmation/dest_id=<str:destId>/hotelName=<str:hotelName>/hotelId=<str:hotelId>/roomKey=<str:roomKey>/roomType=<str:roomType>', views.confirmation, name="confirmation"),
     path('transaction-complete/', views.transactionComplete, name="transaction-complete"),
-
+    
     #temporary paths below for database transition:
     path('hotellistings/dest_id=<str:destId>', views.hotelCards, name="hotel-cards"), #temporary, will be deleted after transition is settled
     path('hotellistings/dest_id=<str:destId>/hotelName=<str:hotelName>/hotelId=<str:hotelId>', views.RoomList, name="room-list-test-api"), #temporary, will be deleted after transition is settled
